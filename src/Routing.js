7c76e444f82563c3789cb1dbe2aaa8f191405577
saeed farhang <saeedfarhang.w@gmail.com>
2021-03-10 04:30:56 +0330

    c

--- src/Routing.js
+++ src/Routing.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 export default function Routing(props) {
   return (
-    <HashRouter>
+    <Router>
       <NavBar />
       <Switch>
         <Route exact path="/">
           <Home />
         </Route>
       </Switch>
-    </HashRouter>
+    </Router>
   );
 }
